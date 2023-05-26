7818f6bb12157bf14724238b6390f8f89e47e951
Vincent-von-Schmidt <vincentvonschmidt@gmail.com>
2023-05-26 10:56:44 +0200

    new class diagramm and error fixing in tex

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import asyncio
 import sys
 import pygame
 import _thread
+from asyncqt import QEventLoop
 from PyQt6.QtWidgets import (
     QApplication,
     QWidget,
@@ -31,37 +32,26 @@ class Window(QFrame):
         self.show()
 
 
-# async def test() -> None:
-#     for i in range(1000):
-#         print(f"{i = }")
-#         await asyncio.sleep(0.1)
-
 async def test() -> None:
     for i in range(1000):
         print(f"{i = }")
         await asyncio.sleep(0.1)
 
-# async def exec(app: QApplication) -> None:
-#     app.exec()
-
-# async def qt() -> None:
-#     app: QApplication = QApplication(sys.argv)
-#     window: Window = Window()
-#
-#     await exec(app)
-
-def qt() -> None:
+async def qt() -> None:
     app: QApplication = QApplication(sys.argv)
-    window: Window = Window()
+    loop: QEventLoop = QEventLoop(app)
+    asyncio.set_event_loop(loop)
 
-    app.exec()
+    with loop:
+        window: Window = Window()
+        loop.run_forever()
 
-# async def main() -> None:
-#     async with asyncio.TaskGroup() as task_group:
-#         task_group.create_task(test())
-#         task_group.create_task(qt())
+    # await app.exec()
+
+async def main() -> None:
+    async with asyncio.TaskGroup() as task_group:
+        task_group.create_task(test())
+        task_group.create_task(qt())
 
 if __name__ == "__main__":
-    # asyncio.run(main())
-    _thread.start_new_thread(qt, ())
-    _thread.start_new_thread(test, ())
+    asyncio.run(main())

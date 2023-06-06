10b08c75c544ac7bc60cb32a543e3499478ed44a
Vincent-von-Schmidt <vincentvonschmidt@gmail.com>
2023-06-06 10:21:21 +0200

    button-tests

--- src/button.py
+++ src/button.py
@@ -8,10 +8,13 @@ class Button:
 
         self.text: str = text
         self.clicked: function = clicked
+
+        self.font: pygame.font.Font = pygame.font.SysFont( const.FONT, 10 )
         
         self.surface: pygame.surface.Surface = pygame.surface.Surface(
             (const.BOARD_RESOLUTION, const.BOARD_RESOLUTION / 8)
         )
 
     def render(self) -> None:
-        ...
+
+        self.surface.fill( (111, 196, 213) )
--- src/const.py
+++ src/const.py
@@ -3,3 +3,4 @@ RATIO: tuple[int, int] = (3, 3) # for chess board pattern -> only uneven numbers
 BOARD_RESOLUTION: int = 400
 BOARD_COLOR_A: tuple[int, int, int] = (0, 0, 0)
 BOARD_COLOR_B: tuple[int, int, int] = (255, 255, 255)
+FONT: str = "Arial"

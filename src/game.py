826e81c64b25c896dcc61286ec78707bb6225300
Vincent-von-Schmidt <vincentvonschmidt@gmail.com>
2023-06-21 19:57:18 +0200

    comments

--- src/game.py
+++ src/game.py
@@ -157,6 +157,9 @@ class Game:
 
                             # highlight the possible turns
                             for turn in self.position.get_possible_turns():
+
+                                # highlight only the possible turns of the tile the 
+                                # currently selected figure is on
                                 if self.map.get_tile_by_id( turn[0] ) == self.highlighted_figure.get_tile():
                                     highlight_tile: map.Tile = self.map.get_tile_by_id( turn[1] )
                                     highlight_tile.set_highlight( True )
